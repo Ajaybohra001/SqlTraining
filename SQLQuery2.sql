2b169e9a3d85293606b726d148b64b59d99b6067
Ajaybohra001 <147503365+Ajaybohra001@users.noreply.github.com>
2023-10-19 15:56:55 +0530

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -21,4 +21,29 @@ Select max(Emp_age) from tbl_Emp
 Select upper(Emp_name) from tbl_Emp
 Select substring(Emp_name,1,2) from tbl_Emp
 
+--group by and having clause
+
+
+Create Table sales (
+    id INT,
+    product_name VARCHAR(50),
+    category VARCHAR(50),
+    price DECIMAL(10, 2)
+);
+
+
+Insert Into  sales (id, product_name, category, price)
+Values (1, 'Product A', 'Category 1', 10.50),
+       (2, 'Product B', 'Category 1', 15.75),
+       (3, 'Product C', 'Category 2', 20.00),
+       (4, 'Product D', 'Category 2', 12.25),
+       (5, 'Product E', 'Category 3', 8.50),
+       (6, 'Product F', 'Category 3', 18.00);
+
+Select category, AVG(price) AS average_price
+From sales
+group  by category
+having AVG(price) > 15.00;
+
+
 
